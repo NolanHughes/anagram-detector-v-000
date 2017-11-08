f872ce830f810cfc889ec16f649bc8d4482c8968
Nolan Hughes <nhughes987@gmail.com>
2017-11-08 17:26:09 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,22 +1,24 @@
 # Your code goes here!
 # class Anagram
 #   attr_accessor :word
-# 
+#
 #   def initialize(word)
 #     @word = word
 #   end
-# 
+#
 #   def match(words)
 #     array = []
-# 
+#
 #     words.each do |w|
 #       if name.split("").sort == w.split("").sort
 #         array << w
 #       end
 #     end
 #   end
-# 
+#
 # end
+
+require 'pry'
 
 def match(words)
   array = []
@@ -31,3 +33,4 @@ end
 words = ["hello", "jessie"]
 name = "ssejie"
 
+binding.pry
